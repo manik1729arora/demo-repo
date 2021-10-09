00c4dbb3e4eaf5418c0f1c201056c4c12706054c
manik1729arora <88608399+manik1729arora@users.noreply.github.com>
2021-10-10 00:02:40 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using std::cout; 
+using std::endl; 
+
+int main()
+{
+	char name[]="Manik Arora"; 
+	int age=20; 
+
+	cout<<"His name is "<<name<<endl; 
+
+	cout<<"His age is "<<age; 
+
+	return 0; 
+}
